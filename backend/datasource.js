cf566465084cfbb5a762183cb6dd2334838a6819
El Hassan EL BAHLOUL <63402222+AiolioSenpai@users.noreply.github.com>
2026-03-30 19:34:58 +0200

    adding schema to TypeORM datasource config

--- backend/datasource.js
+++ backend/datasource.js
@@ -5,5 +5,6 @@ export const appDataSource = new DataSource({
   url: process.env.DATABASE_URL,
   synchronize: true,
   ssl: { rejectUnauthorized: false },
+  schema: 'public',
   entities: ['entities/*.js'],
 });
